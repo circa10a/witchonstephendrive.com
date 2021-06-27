111d79da0dcccfde97dd4a9e286ed9b5539a4795
circa10a <caleblemoine@gmail.com>
2021-06-27 08:32:33 -0500

    add sound endpoint

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"github.com/amimof/huego"
 	_ "github.com/circa10a/witchonstephendrive.com/api" // import generated docs.go
 	"github.com/circa10a/witchonstephendrive.com/internal/colors"
+	"github.com/circa10a/witchonstephendrive.com/internal/config"
 	"github.com/circa10a/witchonstephendrive.com/pkg/utils"
 	"github.com/labstack/echo/v4"
 	log "github.com/sirupsen/logrus"
@@ -15,6 +16,11 @@ import (
 
 // ColorResponse responds with success or failed status string
 type ColorResponse struct {
+	Status string
+}
+
+// SoundResponse responds with success or failed status string
+type SoundResponse struct {
 	Status string
 }
 
@@ -49,8 +55,22 @@ func colorHandler(c echo.Context) error {
 	})
 }
 
+// :sound godoc
+// @Summary Play sound via assistant relay
+// @Description Play halloween sound supported sound defined in sound map
+// @Produce json
+// @Success 200 {object} SoundResponse
+// @Failure 400 {object} SoundResponse
+// @Router /sound/{sound} [post]
+// @Param sound path string true "Sound to play"
+func soundHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, SoundResponse{
+		Status: "success",
+	})
+}
+
 // Routes instantiates all of the listening context paths
-func Routes(e *echo.Echo, hueLights []int, bridge *huego.Bridge, frontendAssets fs.FS, apiDocAssets fs.FS) {
+func Routes(e *echo.Echo, witchConfig config.WitchConfig, frontendAssets fs.FS, apiDocAssets fs.FS) {
 	frontendHTTPFS, err := utils.ConvertEmbedFsDirToHTTPFS(frontendAssets, "web")
 	if err != nil {
 		log.Error(err)
@@ -68,8 +88,14 @@ func Routes(e *echo.Echo, hueLights []int, bridge *huego.Bridge, frontendAssets 
 	// Route to change lights
 	e.POST("/color/:color", colorHandler, func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			c.Set("hueLights", hueLights)
-			c.Set("bridge", bridge)
+			c.Set("hueLights", witchConfig.HueLights)
+			c.Set("bridge", witchConfig.Bridge)
+			return next(c)
+		}
+	})
+
+	e.POST("/sound/:sound", soundHandler, func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
 			return next(c)
 		}
 	})
