965b6e606b1e7a04766dc89c0f0b1d66319da36c
Caleb Lemoine <21261388+circa10a@users.noreply.github.com>
2021-08-01 11:54:45 -0500

    use pub/sub implementation for sounds (#2)

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"embed"
-	"fmt"
 
-	"github.com/amimof/huego"
-	"github.com/circa10a/witchonstephendrive.com/internal/config"
-	"github.com/circa10a/witchonstephendrive.com/internal/routes"
-	"github.com/go-resty/resty/v2"
-	"github.com/kelseyhightower/envconfig"
-	"github.com/labstack/echo-contrib/prometheus"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	log "github.com/sirupsen/logrus"
+	"github.com/circa10a/witchonstephendrive.com/cmd"
 )
 
 //go:embed web
@@ -32,48 +23,5 @@ var apiDocAssets embed.FS
 // @BasePath /
 // @Schemes https
 func main() {
-	// Setup config
-	var witchConfig config.WitchConfig
-	err := envconfig.Process("witch", &witchConfig)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// Hue config
-	// Find hue bridge ip
-	hueBridge, err := huego.Discover()
-	if err != nil {
-		log.Fatal(err)
-	}
-	witchConfig.Bridge = hueBridge
-	// Authenticate against bridge api
-	witchConfig.Bridge.Login(witchConfig.HueUser)
-	// Store all light data to be used later
-	for _, lightID := range witchConfig.HueLights {
-		light, err := witchConfig.Bridge.GetLight(lightID)
-		if err != nil {
-			log.Fatal(err)
-		}
-		witchConfig.HueLightsStructs = append(witchConfig.HueLightsStructs, *light)
-	}
-
-	// Create client to be used with assistant relay
-	assitantRelatEndpoint := fmt.Sprintf("%v:%v", witchConfig.AssistantRelayHost, witchConfig.AssistantRelayPort)
-	witchConfig.Client = resty.New().SetHostURL(assitantRelatEndpoint).SetHeader("Content-Type", "application/json")
-
-	// New instance of echo
-	e := echo.New()
-	// Prometheus metrics
-	if witchConfig.Metrics {
-		prometheus.NewPrometheus("echo", nil).Use(e)
-	}
-	// Use logging middleware
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "[${time_rfc3339}] ${status} ${method} ${path} (${remote_ip}) ${latency_human}\n",
-		Output: e.Logger.Output(),
-	}))
-	// Declare routes
-	routes.Routes(e, witchConfig, frontendAssets, apiDocAssets)
-	// Start App
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", witchConfig.Port)))
+	cmd.Execute(frontendAssets, apiDocAssets)
 }
