9b1b4e89a8f2d49284330f41a71a8ba1b88b9ecb
circa10a <caleblemoine@gmail.com>
2021-09-25 15:01:24 -0500

    explicitly set colormode

--- controllers/colors/colors.go
+++ controllers/colors/colors.go
@@ -13,6 +13,8 @@ import (
 type ColorMap map[string]huego.State
 
 var maxBrightness uint8 = 254
+var colorMode string = "xy"
+var defaultEffect string = "none"
 
 // SupportedColors hold a list of supported colors
 var SupportedColors = getSupportedColors()
@@ -20,77 +22,86 @@ var SupportedColors = getSupportedColors()
 // Colors hold all the supported colors' states
 var Colors = ColorMap{
 	"blue": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.1396,
 			0.0686,
 		},
 	},
 	"green": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.1723,
 			0.6866,
 		},
 	},
 	"orange": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.6424,
 			0.3523,
 		},
 	},
 	"pink": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.3924,
 			0.1634,
 		},
 	},
 	"purple": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.2027,
 			0.0715,
 		},
 	},
 	"rainbow": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "colorloop",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    "colorloop",
 	},
 	"red": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.6786,
 			0.3126,
 		},
 	},
 	"teal": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.1527,
 			0.2144,
 		},
 	},
 	"yellow": {
-		On:     true,
-		Bri:    maxBrightness,
-		Effect: "none",
+		On:        true,
+		Bri:       maxBrightness,
+		ColorMode: colorMode,
+		Effect:    defaultEffect,
 		Xy: []float32{
 			0.5145,
 			0.4691,
